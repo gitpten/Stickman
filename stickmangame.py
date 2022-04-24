8c44378a6bf8c2deeaf5a197e4d8324e09c32f67
Balytsky Igor <ibalyts@live.com>
2022-04-24 23:20:37 +0300

    P2 difficult animation

--- stickmangame.py
+++ stickmangame.py
@@ -39,32 +39,58 @@ class Sprite:
 class Stickman(Sprite):
     def __init__(self, game: Game, coords=(W / 2, H / 2), speed=(0, 0)) -> None:
         super().__init__(game, coords, speed)
-        self.images = [
-            PhotoImage(file='figure-L1.png'),
-            PhotoImage(file='figure-R1.png')
-        ]
+        self.images = self.my_images(15)
+        self.stand_img = PhotoImage(file=f"Stand.png")
         x, y = self.coords
-        self.obj = self.canvas.create_image(x, y, image=self.images[0])
+        self.obj = self.canvas.create_image(x, y, image=self.stand_img)
+        self.dir = None
+        self.frame = 0
         self.canvas.bind_all("<KeyPress>", self.force)
         self.canvas.bind_all("<KeyRelease>", self.unforce)
+
+    def my_images(self, num):
+        img = []
+        for i in range(num):
+            img.append(PhotoImage(file=f"L{i + 1}.png"))
+        for i in range(num):
+            img.append(PhotoImage(file=f"R{i + 1}.png"))
+        return img
+
     
     def force(self, event):
         key = event.keysym
         sx, sy = self.speed
         if key == "Left":
             self.speed = (-SPD, sy)
-            self.canvas.itemconfig(self.obj, image = self.images[0])
+            self.dir = 0            
         if key == "Right":
             self.speed = (SPD, sy)
-            self.canvas.itemconfig(self.obj, image = self.images[1])
+            self.dir = 1
 
     def unforce(self, event):
         key = event.keysym
         sx, sy = self.speed
         if key == "Left":
             self.speed = (0, sy)
+            self.frame = 0
+            self.dir = None
         if key == "Right":
             self.speed = (0, sy)
+            self.frame = 0
+            self.dir = None
+    
+    def move(self):
+        super().move()
+        self.frame += 0.5
+        self.animate()
+
+    def animate(self):
+        if self.dir == None:
+            self.canvas.itemconfig(self.obj, image=self.stand_img)
+            return
+        num = int(self.frame) % 15 + self.dir * 15 
+        self.canvas.itemconfig(self.obj, image=self.images[num])
+
 
 g = Game()
 while g.run:
