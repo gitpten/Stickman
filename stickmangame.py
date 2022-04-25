c3cca59dd6f0f271686fcf61f3e26c477a334779
Balytsky Igor <ibalyts@live.com>
2022-04-25 22:55:34 +0300

    P3 jump animation bad need to improove

--- stickmangame.py
+++ stickmangame.py
@@ -1,10 +1,8 @@
-from math import ceil
 from time import sleep
 from tkinter import Canvas, PhotoImage, Tk, mainloop
-from turtle import speed
 
-W = 500
-H = 500
+
+W, H = 500, 500
 SPD = 5
 
 class Game:
@@ -13,41 +11,39 @@ class Game:
         self.canvas = Canvas(self.win, width=W, height=H)
         self.canvas.pack()
         self.run = True
-        self.sprites = [Stickman(self, (20, H - 20))]
-
+        self.sprites = [Stickman(self)]
+    
     def tick(self):
         for sprite in self.sprites:
             sprite.update()
-            self.canvas.update()
-
+        self.canvas.update()
+        sleep(0.02)
 
 
 class Sprite:
-    def __init__(self, game: Game, coords = (W / 2, H / 2), speed = (0, 0)) -> None:
-        self.game = game
-        self.canvas = self.game.canvas
-        self.coords = coords
-        self.speed = speed
+    def __init__(self, g: Game, x = W / 2, y = H / 2, speedx = 0, speedy = 0) -> None:
+        self.game = g
+        self.canvas = g.canvas
+        self.x, self.y = x, y
+        self.speedx, self.speedy = speedx, speedy
         self.obj = None
     
     def update(self):
         self.move()
 
     def move(self):
-        sx, sy = self.speed
-        x, y = self.coords
-        self.coords = (x + sx, y - sy)
-        self.canvas.coords(self.obj, self.coords[0], self.coords[1])
-
+        self.x += self.speedx
+        self.y -= self.speedy
+        self.canvas.coords(self.obj, self.x, self.y)
+        
 
 class Stickman(Sprite):
-    def __init__(self, game: Game, coords=(W / 2, H / 2), speed=(0, 0)) -> None:
-        super().__init__(game, coords, speed)
-        self.images = self.my_images(15)
-        self.stand_img = PhotoImage(file=f"Stand.png")
-        x, y = self.coords
-        self.obj = self.canvas.create_image(x, y, image=self.stand_img)
+    def __init__(self, g: Game, x = W / 2, y = H / 2, speedx=0, speedy=0) -> None:
+        super().__init__(g, x, y, speedx, speedy)
+        self.costumes = self.get_costumes()
         self.frame = 0
+        self.flying = True
+        self.obj = self.canvas.create_image(self.x, self.y, image = self.costumes[-1], anchor='center')
         self.canvas.bind_all("<KeyPress>", self.force)
         self.canvas.bind_all("<KeyRelease>", self.unforce)
 
@@ -60,51 +56,76 @@ class Stickman(Sprite):
         return img
 
     
+    def get_costumes(self):
+        imgs = []
+        for i in range(15):
+            imgs.append(PhotoImage(file=f"L{i + 1}.png"))
+        for i in range(15):
+            imgs.append(PhotoImage(file=f"R{i + 1}.png"))
+        imgs.append(PhotoImage(file=f"LJ1.png"))
+        imgs.append(PhotoImage(file=f"LJ2.png"))
+        imgs.append(PhotoImage(file=f"RJ1.png"))
+        imgs.append(PhotoImage(file=f"RJ2.png"))
+        imgs.append(PhotoImage(file=f"Stand.png"))
+        return imgs
+    
     def force(self, event):
         key = event.keysym
-        sx, sy = self.speed
         if key == "Left":
-            self.speed = (-SPD, sy)     
+            self.speedx = -SPD
         if key == "Right":
-            self.speed = (SPD, sy)
-        if key == "Up":
-            self.speed = (sx, 20)     
+            self.speedx = SPD   
+        if key == "Up" and not self.flying:
+            self.speedy = 20 
+            self.flying = True
 
     def unforce(self, event):
         key = event.keysym
-        sx, sy = self.speed
         if key == "Left":
-            self.speed = (0, sy)
-            self.frame = 0
+            self.speedx = 0  
+            self.frame = 0          
         if key == "Right":
-            self.speed = (0, sy)
-            self.frame = 0
-    
+            self.speedx = 0
+            self.frame = 0           
+
+    def switchcostume(self, num):
+        self.canvas.itemconfig(self.obj, image = self.costumes[num]) 
+
     def update(self):
-        super().update()
-        sx, sy = self.speed
-        self.speed = (sx, sy - 1)
-        self.check()    
-        self.frame += 1
+        super().update()  
+        self.speedy -= 1   
+        self.check()   
         self.animate()
-
-    def check(self):
-        while(self.coords[1] > H - 15):
-            self.speed = (self.speed[0], 0)
-            self.coords = (self.coords[0], self.coords[1] - 1)
-        self.canvas.coords(self.obj, self.coords) 
-
+        self.frame += 1
+    
     def animate(self):
-        if self.speed[0] == 0:
-            self.canvas.itemconfig(self.obj, image=self.stand_img)
+        if self.speedx == 0:
+            self.switchcostume(-1)
             return
-        num = self.frame % 15 if self.speed[0] < 0 else self.frame % 15 + 15
-        self.canvas.itemconfig(self.obj, image=self.images[num])
-
+        if self.speedx < 0:
+            if self.flying and self.speedy > 0:
+                self.switchcostume(-5)
+            elif self.flying:
+                self.switchcostume(-4)
+            else:
+                self.switchcostume(self.frame % 15)
+        else:
+            if self.flying and self.speedy > 0:
+                self.switchcostume(-3)
+            elif self.flying:
+                self.switchcostume(-2)
+            else:
+                self.switchcostume(self.frame % 15 + 15)
+    
+    def check(self):
+        while self.y > H - 15:
+            self.speedy = 0
+            self.flying = False
+            self.y -= 1
+        self.canvas.coords(self.obj, self.x, self.y)
 
 g = Game()
 while g.run:
     g.tick()
-    sleep(0.02)
 
 mainloop()
