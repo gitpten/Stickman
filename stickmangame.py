8932b45ca7953543b5dea0248c3e66f686bee4db
Balytsky Igor <ibalyts@live.com>
2022-04-25 11:58:57 +0300

    P2 animation & speedy and collision with floor

--- stickmangame.py
+++ stickmangame.py
@@ -15,7 +15,7 @@ class Game:
     
     def tick(self):
         for sprite in self.sprites:
-            sprite.move()
+            sprite.update()
         self.canvas.update()
         sleep(0.02)
 
@@ -28,45 +28,75 @@ class Sprite:
         self.speedx, self.speedy = speedx, speedy
         self.obj = None
     
+    def update(self):
+        self.move()
+
     def move(self):
-        self.canvas.move(self.obj, self.speedx, self.speedy)
+        self.x += self.speedx
+        self.y -= self.speedy
+        self.canvas.coords(self.obj, self.x, self.y)
         
 
 class Stickman(Sprite):
     def __init__(self, g: Game, x = W / 2, y = H / 2, speedx=0, speedy=0) -> None:
         super().__init__(g, x, y, speedx, speedy)
-        self.costumes = [
-            PhotoImage(file="figure-L1.png"),
-            PhotoImage(file="figure-R1.png"),
-        ]
-        self.obj = self.canvas.create_image(self.x, self.y, image = self.costumes[1], anchor='center')
+        self.costumes = self.get_costumes()
+        self.frame = 0
+        self.obj = self.canvas.create_image(self.x, self.y, image = self.costumes[-1], anchor='center')
         self.canvas.bind_all("<KeyPress>", self.force)
         self.canvas.bind_all("<KeyRelease>", self.unforce)
+    
+    def get_costumes(self):
+        imgs = []
+        for i in range(15):
+            imgs.append(PhotoImage(file=f"L{i + 1}.png"))
+        for i in range(15):
+            imgs.append(PhotoImage(file=f"R{i + 1}.png"))
+        imgs.append(PhotoImage(file=f"Stand.png"))
+        return imgs
     
     def force(self, event):
         key = event.keysym
         if key == "Left":
             self.speedx = -SPD
-            self.switchcostume(0)
         if key == "Right":
-            self.speedx = SPD     
-            self.switchcostume(1)       
+            self.speedx = SPD   
         if key == "Up":
-            self.speedy = -20    
+            self.speedy = 20 
 
     def unforce(self, event):
         key = event.keysym
         if key == "Left":
-            self.speedx = 0            
+            self.speedx = 0  
+            self.frame = 0          
         if key == "Right":
-            self.speedx = 0           
+            self.speedx = 0
+            self.frame = 0           
 
     def switchcostume(self, num):
         self.canvas.itemconfig(self.obj, image = self.costumes[num]) 
 
-    def move(self):
-        super().move()
-        self.speedy += 1   
+    def update(self):
+        super().update()  
+        self.speedy -= 1   
+        self.check()   
+        self.animate()
+        self.frame += 1
+    
+    def animate(self):
+        if self.speedx == 0:
+            self.switchcostume(-1)
+            return
+        if self.speedx < 0:
+            self.switchcostume(self.frame % 15)
+        else:
+            self.switchcostume(self.frame % 15 + 15)
+    
+    def check(self):
+        while self.y > H - 15:
+            self.speedy = 0
+            self.y -= 1
+        self.canvas.coords(self.obj, self.x, self.y)
 
 g = Game()
 while g.run:
