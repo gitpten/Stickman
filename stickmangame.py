336056059c2f424648db64028f015fd39f5b9661
Balytsky Igor <ibalyts@live.com>
2022-05-02 00:58:46 +0300

    P3 plathform collition

--- stickmangame.py
+++ stickmangame.py
@@ -1,5 +1,8 @@
+from operator import truth
 from time import sleep
 from tkinter import Canvas, PhotoImage, Tk, mainloop
+
+from pkg_resources import get_platform
 
 
 W, H = 500, 500
@@ -11,7 +14,19 @@ class Game:
         self.canvas = Canvas(self.win, width=W, height=H)
         self.canvas.pack()
         self.run = True
-        self.sprites = [Stickman(self)]
+        self.level = 1
+        self.sprites = self.get_platforms() + [Stickman(self)]
+    
+    def get_platforms(self):
+        ps = []
+        if self.level == 1:
+            ps.append(Plathform(self, 2, 1, width=2))
+            ps.append(Plathform(self, 3, 4))
+            ps.append(Plathform(self, 1, 5, width=2))
+            ps.append(Plathform(self, 6, 1))
+            ps.append(Plathform(self, 5, 8, width=3))
+            ps.append(Plathform(self, 3, 2, width=2))
+        return ps
     
     def tick(self):
         for sprite in self.sprites:
@@ -35,7 +50,13 @@ class Sprite:
         self.x += self.speedx
         self.y -= self.speedy
         self.canvas.coords(self.obj, self.x, self.y)
-        
+
+class Plathform(Sprite):
+    def __init__(self, g: Game, x, y, speedx=0, speedy=0, width = 1) -> None:
+        super().__init__(g, x * W / 10, y * H / 10, speedx, speedy) 
+        self.img = PhotoImage(file=f"P{width}.png") 
+        self.obj = self.canvas.create_image(self.x, self.y, image = self.img, anchor='center')
+        self.width = 30 * width   
 
 class Stickman(Sprite):
     def __init__(self, g: Game, x = W / 2, y = H / 2, speedx=0, speedy=0) -> None:
@@ -110,11 +131,20 @@ class Stickman(Sprite):
             self.switchcostume(self.frame % 15 + 15 * right)
     
     def check(self):
-        while self.y > H - 15:
+        while self.collide() or self.y > H - 15:
             self.speedy = 0
             self.flying = False
             self.y -= 1
         self.canvas.coords(self.obj, self.x, self.y)
+    
+    def collide(self):
+        for sprite in self.game.sprites:
+            if sprite == self:
+                 continue
+            if self.speedy <= 0 and self.y + 15 > sprite.y > self.y - 15 \
+                and sprite.x - sprite.width / 2 < self.x < sprite.x + sprite.width / 2:
+                return True
+        return False
 
 g = Game()
 while g.run:
