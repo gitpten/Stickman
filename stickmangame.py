2579ea3b06d10a2f509e21fd6e2bb707cd427478
gitpten <gm.pten@gmail.com>
2022-05-06 19:58:39 +0300

    P4 door and new level

--- stickmangame.py
+++ stickmangame.py
@@ -12,7 +12,8 @@ class Game:
         self.canvas.pack()
         self.run = True
         self.level = 1
-        self.sprites = self.get_platforms() + [Stickman(self)]
+        self.stickman = Stickman(self)
+        self.sprites = self.get_platforms() + [self.stickman]
     
     def get_platforms(self):
         ps = []
@@ -23,7 +24,29 @@ class Game:
             ps.append(Plathform(self, 6, 1))
             ps.append(Plathform(self, 5, 8, width=3))
             ps.append(Plathform(self, 3, 2, width=2))
+        elif self.level == 2:
+            ps.append(Plathform(self, 1, 2, width=2))
+            ps.append(Plathform(self, 4, 3))
+            ps.append(Plathform(self, 5, 1, width=2))
+            ps.append(Plathform(self, 1, 6))
+            ps.append(Plathform(self, 8, 5, width=3))
+            ps.append(Plathform(self, 2, 8))
+        
+        ps.insert(0, Door(ps[-1]))
         return ps
+    
+    def newlevel(self):
+        if self.level == 2:
+            self.gameover()
+            return
+        self.canvas.delete('all')
+        self.level += 1
+        self.stickman = Stickman(self)
+        self.sprites = self.get_platforms() + [self.stickman]
+    
+    def gameover(self):
+        self.run = False
+        self.canvas.create_text(W / 2, H / 2, font=('Arial', 30), text = 'G A M E   O V E R', anchor = 'center')
     
     def tick(self):
         for sprite in self.sprites:
@@ -48,12 +71,28 @@ class Sprite:
         self.y -= self.speedy
         self.canvas.coords(self.obj, self.x, self.y)
 
+
+
 class Plathform(Sprite):
     def __init__(self, g: Game, x, y, speedx=0, speedy=0, width = 1) -> None:
         super().__init__(g, x * W / 10, y * H / 10, speedx, speedy) 
         self.img = PhotoImage(file=f"P{width}.png") 
         self.obj = self.canvas.create_image(self.x, self.y, image = self.img, anchor='center')
         self.width = 30 * width   
+
+
+
+class Door(Sprite):
+    def __init__(self, p: Plathform) -> None:
+        super().__init__(p.game, p.x, p.y, 0, 0)
+        self.img_closed = PhotoImage(file=f"door1.png") 
+        self.img_opened = PhotoImage(file=f"door2.png") 
+        self.obj = self.canvas.create_image(self.x, self.y, image = self.img_closed, anchor='s')
+        self.width = 30
+
+
+
+
 
 class Stickman(Sprite):
     def __init__(self, g: Game, x = W / 2, y = H / 2, speedx=0, speedy=0) -> None:
@@ -128,19 +167,24 @@ class Stickman(Sprite):
             self.switchcostume(self.frame % 15 + 15 * right)
     
     def check(self):
+        if type(self.collide()) is Door:
+            self.game.newlevel()
+            return
         while self.collide() or self.y > H - 15:
             self.speedy = 0
             self.flying = False
             self.y -= 1
         self.canvas.coords(self.obj, self.x, self.y)
+
     
     def collide(self):
         for sprite in self.game.sprites:
             if sprite == self:
                  continue
+
             if self.speedy <= 0 and self.y + 15 > sprite.y > self.y - 15 \
                 and sprite.x - sprite.width / 2 < self.x < sprite.x + sprite.width / 2:
-                return True
+                return sprite
         return False
 
 g = Game()
